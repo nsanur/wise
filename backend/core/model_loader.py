6492463c583e9c335bc5797e85f155495bd9b492
Nisanur <103697226+nsanur@users.noreply.github.com>
2025-06-09 00:25:28 +0300

    Update model_loader.py

--- backend/core/model_loader.py
+++ backend/core/model_loader.py
@@ -1,32 +1,33 @@
 #  core/model_loader.py
 
+from pathlib import Path
 import torch
 import os
-import pathlib
-from pathlib import Path
-pathlib.PosixPath = pathlib.WindowsPath
 
 def load_models():
+    base_path = Path(__file__).resolve().parent.parent / "models"
+
     model_paths = {
-        'wisePlate': 'models/wisePlate.pt',
-        'wiseSoup': 'models/wiseSoup.pt',
-        'wiseTypeSoup': 'models/wiseTypeSoup.pt',
-        'wiseExtraCls': 'models/wiseExtraCls-yolo5.pt',
-        'wiseExtraTypeCls': 'models/wiseExtraTypeCls-yolo5.pt',
-        'wiseMainCls': 'models/wiseMainCls-yolo5.pt',
-        'wiseMainTypeCls': 'models/wiseMainTypeCls-yolo5.pt',
-        'wiseSideCls': 'models/wiseSideCls-yolo5.pt',
-        'wiseSideTypeCls': 'models/wiseSideTypeCls-yolo5.pt'
+        'wisePlate': base_path / 'wisePlate.pt',
+        'wiseSoup': base_path / 'wiseSoup.pt',
+        'wiseTypeSoup': base_path / 'wiseTypeSoup.pt',
+        'wiseExtraCls': base_path / 'wiseExtraCls-yolo5.pt',
+        'wiseExtraTypeCls': base_path / 'wiseExtraTypeCls-yolo5.pt',
+        'wiseMainCls': base_path / 'wiseMainCls-yolo5.pt',
+        'wiseMainTypeCls': base_path / 'wiseMainTypeCls-yolo5.pt',
+        'wiseSideCls': base_path / 'wiseSideCls-yolo5.pt',
+        'wiseSideTypeCls': base_path / 'wiseSideTypeCls-yolo5.pt'
     }
 
     models = {}
     for model_name, model_path in model_paths.items():
-        if not os.path.exists(model_path):
+        if not model_path.exists():
             print(f"Model dosyası bulunamadı: {model_path}")
             continue
         print(f"Loading model: {model_name}")
-        model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path=str(model_path))  # str() ile aktar
         model.eval()
         models[model_name] = model
 
     return models
+
